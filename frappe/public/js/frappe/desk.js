80b3a02e6d747362763677aaa538168e8de4263b
NahuelOperto <Nahuel713.-@hotmail.com>
2020-01-13 11:02:29 -0300

    Sin cartel de update

--- frappe/public/js/frappe/desk.js
+++ frappe/public/js/frappe/desk.js
@@ -80,7 +80,7 @@ frappe.Application = Class.extend({
 			this.show_notes();
 		}
 
-		this.show_update_available();
+		// this.show_update_available();
 
 		if(frappe.ui.startup_setup_dialog && !frappe.boot.setup_complete) {
 			frappe.ui.startup_setup_dialog.pre_show();
